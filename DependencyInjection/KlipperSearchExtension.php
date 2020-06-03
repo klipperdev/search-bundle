c5be2b2806ac14df9ac13fc0840a37447a4055e3
François Pluchino <francois.pluchino@klipper.dev>
2020-06-03 19:20:31 +0200

    Add php type hints and update phpdoc

--- DependencyInjection/KlipperSearchExtension.php
+++ DependencyInjection/KlipperSearchExtension.php
@@ -24,8 +24,6 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 class KlipperSearchExtension extends Extension
 {
     /**
-     * {@inheritdoc}
-     *
      * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container): void
